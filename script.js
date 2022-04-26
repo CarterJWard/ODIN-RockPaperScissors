ce4a8a01e78055882f561941b3e62202e3c25d4f
CarterJWard <cjayward63@gmail.com>
2022-04-26 10:55:47 +1000

    Main game completed Scoreboards completed and working Winning and losing messages added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let cScore = 0;
+let round = 1;
+let maxRounds = 5;
+
 const computerSelection = () => {
     let options = ['rock', 'paper', 'scissors']
     let selection = Math.floor(Math.random() * (2 - 0 + 1) + 0)
@@ -35,18 +40,40 @@ const playRound = (player, computer) => {
 
 const winGame = (player, computer) => {
     console.log("you won " + player + ' beats ' + computer)
+    round++;
+    playerScore++;
 }
 
 const loseGame = (player, computer) => {
     console.log("you lost " + computer + " beats " + player)
+    round++;
+    cScore++;
 }
 
 const tiedGame = () => {
     console.log('Game Tied')
 }
 
-const game = () => {
-    let playerSelection = prompt('enter your choice')
-    console.log(playerSelection + ' player')
-    playRound(playerSelection, computerSelection())
+const updateScoreboard = () => {
+    document.getElementById('playerPoint').innerHTML = 'Player Score: ' +playerScore
+    document.getElementById('computerPoint').innerHTML = 'Computer Score: ' + cScore
+    document.getElementById('roundCount').innerHTML = 'Round: '+round
 }
+
+const game = () => {
+    while(round <= maxRounds ) {
+        let playerSelection = prompt('enter your choice')
+        console.log(playerSelection + ' player')
+        playRound(playerSelection.toLowerCase(), computerSelection())
+        updateScoreboard()
+    }
+    if (playerScore > cScore) {
+        document.getElementById('roundCount').innerHTML = 'Congratulations, you won'
+    } else {
+        document.getElementById('roundCount').innerHTML = 'Better luck next time :('
+    }
+}
+
+updateScoreboard()
+game()
+
