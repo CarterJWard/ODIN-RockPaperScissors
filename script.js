891c53038296b4fe4881570bfd369a228022fc22
CarterJWard <cjayward63@gmail.com>
2022-04-26 10:27:30 +1000

    Basic game functions core logic added with win lose and tie conditions

---
+++ script.js
@@ -0,0 +1,50 @@
+const computerSelection = () => {
+    let options = ['rock', 'paper', 'scissors']
+    let selection = Math.floor(Math.random() * (2 - 0 + 1) + 0)
+    return options[selection];
+}
+
+const playRound = (player, computer) => {
+    if (player == computer) {
+        tiedGame()
+    } else {
+        if (player == 'rock') {
+            if (computer = 'paper') {
+                loseGame(player, computer)
+            } else {
+                winGame(player, computer)
+            }
+        }
+        if (player == 'scissors') {
+            if (computer == 'rock') {
+                loseGame(player, computer)
+            } else {
+                winGame(player, computer)
+            }
+        }
+        if (player == 'paper') {
+            if(computer == 'scissors') {
+                loseGame(player, computer)
+            } else {
+                winGame(player,computer)
+            }
+        }
+    }
+}
+
+const winGame = (player, computer) => {
+    console.log("you won " + player + ' beats ' + computer)
+}
+
+const loseGame = (player, computer) => {
+    console.log("you lost " + computer + " beats " + player)
+}
+
+const tiedGame = () => {
+    console.log('Game Tied')
+}
+
+const game = () => {
+    let playerSelection
+    playRound(playerSelection, computerSelection())
+}
