487c148033419a861efeb97d42672e4abd296f2d
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2022-05-09 18:58:49 +1000

    Fully functioing UI Removed prompt for user input in exhange for buttons added round history

--- script.js
+++ script.js
@@ -28,51 +28,73 @@ const playRound = (player, computer) => {
             }
         }
         if (player == 'paper') {
-            if(computer == 'scissors') {
+            if (computer == 'scissors') {
                 loseGame(player, computer)
             } else {
-                winGame(player,computer)
+                winGame(player, computer)
             }
         }
     }
 }
 
+
 const winGame = (player, computer) => {
     console.log("you won " + player + ' beats ' + computer)
+    let scoreboard = document.querySelector('#scoreboard')
+    let addition = document.createElement('h3')
+    addition.textContent = 'You Won! ' + player + ' beats ' + computer
+    scoreboard.appendChild(addition)
     round++;
     playerScore++;
+    updateScoreboard()
 }
 
 const loseGame = (player, computer) => {
     console.log("you lost " + computer + " beats " + player)
+    let scoreboard = document.querySelector('#scoreboard')
+    let addition = document.createElement('h3')
+    addition.textContent = "You lost " + computer + " beats " + player
+    scoreboard.appendChild(addition)
     round++;
     cScore++;
+    updateScoreboard()
 }
 
 const tiedGame = () => {
     console.log('Game Tied')
+    let scoreboard = document.querySelector('#scoreboard')
+    let addition = document.createElement('h3')
+    addition.textContent = 'Game Tied'
+    scoreboard.appendChild(addition)
 }
 
 const updateScoreboard = () => {
-    document.getElementById('playerPoint').innerHTML = 'Player Score: ' +playerScore
-    document.getElementById('computerPoint').innerHTML = 'Computer Score: ' + cScore
-    document.getElementById('roundCount').innerHTML = 'Round: '+round+'/'+maxRounds
+    document.getElementById('player').innerHTML = 'Player Score: ' + playerScore
+    document.getElementById('comp').innerHTML = 'Computer Score: ' + cScore
+    document.getElementById('round').innerHTML = 'Round: ' + round + '/' + maxRounds
+    if (round > maxRounds) {
+        let board = document.querySelector('#round')
+        if (cScore > playerScore) {
+            board.textContent = 'Better luck next time'
+        } else {
+            board.textContent = "congrats you've won"
+        }
+    }
+
 }
 
-const game = () => {
-    while(round <= maxRounds ) {
-        let playerSelection = prompt('enter your choice')
-        console.log(playerSelection + ' player')
-        playRound(playerSelection.toLowerCase(), computerSelection())
-        updateScoreboard()
-    }
-    if (playerScore > cScore) {
-        document.getElementById('roundCount').innerHTML = 'Congratulations, you won'
-    } else {
-        document.getElementById('roundCount').innerHTML = 'Better luck next time :('
-    }
+let rock = document.querySelector('#rock')
+rock.onclick = () => {
+    playRound('rock', computerSelection())
 }
 
+let paper = document.querySelector('#paper')
+paper.onclick = () => {
+    playRound('paper', computerSelection())
+}
+
+let scissors = document.querySelector('#scissors')
+scissors.onclick = () => {
+    playRound('scissors', computerSelection())
+}
 updateScoreboard()
-game()
-
